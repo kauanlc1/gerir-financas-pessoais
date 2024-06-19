21eb43e6771f651ae94cc1bab51d2ffbc083c592
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-19 17:04:57 -0300

    ✨ feat: Implementação do service, controller e repository para Category
    Foram criados os seguintes componentes para a entidade Category:
    - Service: Lógica de negócio para manipulação de Category.
    - Controller: Endpoints para gerenciar as requisições relacionadas a Category.
    - Repository: Integração com a camada de persistência para Category.

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,50 @@
+package org.example.controller;
+
+import org.example.Exceptions.CategoryExistingException;
+import org.example.Exceptions.CategoryNotFoundException;
+import org.example.model.Category;
+import org.example.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/category")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+
+    @GetMapping("/rescue")
+    public Optional<List<Category>> getAllCategoryes() {
+        return Optional.ofNullable(categoryService.findAll());
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> registerCategory(@RequestBody Category category) {
+        try {
+            categoryService.save(category);
+            String descricaoCategoria = category.getName();
+            return ResponseEntity.ok("Categoria '" + descricaoCategoria + "' registrada com sucesso.");
+        } catch (CategoryExistingException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao registrar a categoria " + category.getName(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> removeCategory(@PathVariable(value = "id") Long id) {
+        try {
+            categoryService.deleteById(id);
+            return ResponseEntity.ok("Categoria removida com sucesso.");
+        } catch (CategoryNotFoundException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao remover a categoria.", HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+}
---
+++ src/main/java/org/example/service/CategoryService.java
@@ -0,0 +1,45 @@
+package org.example.service;
+
+import org.example.Exceptions.CategoryExistingException;
+import org.example.Exceptions.CategoryNotFoundException;
+import org.example.model.Category;
+import org.example.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryService {
+    @Autowired
+    CategoryRepository categoryRepository;
+
+    public List<Category> findAll() {
+        return categoryRepository.findAll();
+    }
+
+    public Optional<Category> findById(Long id) {
+        return categoryRepository.findById(id);
+    }
+
+    public void save(Category category) {
+        if (categoryRepository.existsByName(category.getName())) {
+            throw new CategoryExistingException("Categoria com o nome '" + category.getName() + "' já se encontra no sistema.");
+        }
+
+        categoryRepository.save(category);
+    }
+
+    public void deleteById(Long id) {
+        if (!categoryRepository.existsById(id)) {
+            throw new CategoryNotFoundException("Categoria com ID " + id + " não encontrada.");
+        }
+        categoryRepository.deleteById(id);
+    }
+
+    public boolean existsById(Long id) {
+        categoryRepository.existsById(id);
+        return false;
+    }
+}
