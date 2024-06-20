bf5814f615108d597e2a9e29edd21a83d73435cd
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-20 17:35:07 -0300

    ✨ feat: Atualização da TransactionController com novos endpoints e melhorias
    - Adicionado endpoint para obter todas as transações: GET /transactions/rescue.
    - Adicionado endpoint para obter uma transação específica por ID: GET /transactions/rescue/{id}.
    - Adicionado endpoint para criar uma nova transação: POST /transactions/register.
    - Adicionado endpoint para remover uma transação por ID: DELETE /transactions/remove/{id}.
    - Incluída a utilização de ResponseEntity para retornar respostas HTTP adequadas.
    - Melhorias na resposta ao cliente, incluindo mensagens claras de erro e sucesso.

--- src/main/java/org/example/controller/TransactionController.java
+++ src/main/java/org/example/controller/TransactionController.java
@@ -3,9 +3,12 @@ package org.example.controller;
 import org.example.model.Transaction;
 import org.example.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/transactions")
@@ -13,13 +16,34 @@ public class TransactionController {
     @Autowired
     private TransactionService transactionService;
 
-    @PostMapping
-    public Transaction createTransaction(@RequestBody Transaction transaction) {
-        return transactionService.saveTransaction(transaction);
+    @GetMapping("/rescue")
+    public ResponseEntity<?> getAllTransaction() {
+        List<Transaction> transactionsList = transactionService.getAllTransactions();
+        return ResponseEntity.ok(transactionsList);
     }
 
-    @GetMapping
-    public List<Transaction> getAllTransaction() {
-        return transactionService.getAllTransactions();
+    @GetMapping("/rescue/{id}")
+    public ResponseEntity<?> getOneTransaction(@PathVariable(value = "id") Long id) {
+        Optional<Transaction> transaction = transactionService.getTransactionById(id);
+        if (transaction.isPresent()) {
+            return new ResponseEntity<>(transaction.get(), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>("Transaction not found", HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<?> createTransaction(@RequestBody Transaction transaction) {
+        Transaction savedTransaction = transactionService.saveTransaction(transaction);
+        return ResponseEntity.ok(savedTransaction);
+    }
+
+    @DeleteMapping("/remove/{id}")
+    public ResponseEntity<?> removeTransactionById(@PathVariable(value = "id") Long id) {
+        if (!transactionService.removeById(id)) {
+            return new ResponseEntity<>("Ocorreu um erro ao solicitar a remoção da transação com o ID " + id + ".", HttpStatus.BAD_REQUEST);
+        }
+
+        return ResponseEntity.ok("Remoção de transação com o ID " + id + " concluída com sucesso.");
     }
 }
