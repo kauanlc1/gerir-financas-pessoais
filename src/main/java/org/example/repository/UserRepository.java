c83d27850cc910821e486f420453e568e1fd5251
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-25 17:12:45 -0300

    ✨ feat: Adiciona métodos de verificação de existência no UserRepository
    - Adicionados métodos existsByNomeUsuario, existsByEmail, e existsByNome no UserRepository para permitir verificações de existência.
    - Adicionada anotação @Repository para melhor consistência e clareza.

--- src/main/java/org/example/repository/UserRepository.java
+++ src/main/java/org/example/repository/UserRepository.java
@@ -2,10 +2,15 @@ package org.example.repository;
 
 import org.example.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface UserRepository extends JpaRepository<Users, UUID> {
     Optional<Users> findByNomeUsuario(String nomeUsuario);
+    boolean existsByNomeUsuario(String nomeUsuario);
+    boolean existsByEmail(String email);
+    boolean existsByNome(String nome);
 }
