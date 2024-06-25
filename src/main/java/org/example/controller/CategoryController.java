6e1a938ae2528838774c714670795922a9da85d1
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-25 14:11:32 -0300

    ♻️ refactor: Atualiza CategoryController para utilizar CategoryExceptionHandler
    - Removida a lógica de captura de exceções diretamente na CategoryController.
    - Delegada a responsabilidade de tratamento de exceções para o CategoryExceptionHandler.

--- src/main/java/org/example/controller/CategoryController.java
+++ src/main/java/org/example/controller/CategoryController.java
@@ -25,27 +25,15 @@ public class CategoryController {
 
     @PostMapping("/register")
     public ResponseEntity<String> registerCategory(@RequestBody Category category) {
-        try {
             categoryService.save(category);
             String descricaoCategoria = category.getName();
             return ResponseEntity.ok("Categoria '" + descricaoCategoria + "' registrada com sucesso.");
-        } catch (CategoryExistingException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.CONFLICT);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Erro ao registrar a categoria " + category.getName(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
     }
 
 
     @DeleteMapping("/remove/{id}")
-    public ResponseEntity<?> removeCategory(@PathVariable(value = "id") Long id) {
-        try {
-            categoryService.deleteById(id);
-            return ResponseEntity.ok("Categoria removida com sucesso.");
-        } catch (CategoryNotFoundException e) {
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
-        } catch (Exception e) {
-            return new ResponseEntity<>("Erro ao remover a categoria.", HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+    public ResponseEntity<String> removeCategory(@PathVariable(value = "id") Long id) {
+        categoryService.deleteById(id);
+        return ResponseEntity.ok("Categoria removida com sucesso.");
     }
 }
