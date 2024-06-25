f37e62aa6887f9d4923677e3e5ea8facfa201d2f
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-25 11:20:08 -0300

    ♻️ refactor: Atualiza UserController para utilizar UserRecordDTO e adiciona endpoint de atualização parcial
    - Atualizada a UserController para utilizar a nova UserRecordDTO nos endpoints de login.
    - Adicionado endpoint de atualização parcial de usuário com o método PATCH.
    - Melhorada a verificação de credenciais no método de login.

--- src/main/java/org/example/controller/UserController.java
+++ src/main/java/org/example/controller/UserController.java
@@ -1,5 +1,7 @@
 package org.example.controller;
 
+import jakarta.validation.Valid;
+import org.example.dtos.UserRecordDTO;
 import org.example.model.Users;
 import org.example.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 @RestController
@@ -15,22 +19,51 @@ public class UserController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/register")
-    public ResponseEntity<Users> createUser(@RequestBody Users user) {
-        Users savedUsers = userService.saveUser(user);
-        return ResponseEntity.ok(savedUsers);
-    }
     @GetMapping("/rescue")
     public ResponseEntity<List<Users>> getAllUsers() {
         List<Users> ransomedUsers = userService.getAllUsers();
         return ResponseEntity.ok(ransomedUsers);
     }
 
+    @GetMapping("/rescue/{id}")
+    public ResponseEntity<?> getById(@PathVariable(value = "id") UUID id) {
+        Users usuario = userService.findById(id);
+
+        if (usuario != null) {
+            return ResponseEntity.ok(usuario);
+        } else {
+            return ResponseEntity.status(401).body("Não foram encotrados dados vinculados a este ID.");
+        }
+    }
+
+    @PatchMapping("/update/{id}")
+    public ResponseEntity<Users> updateUser(@PathVariable(value = "id") UUID id, @RequestBody Map<String, Object> updates) {
+        Users updatedUser = userService.updateUser(id, updates);
+        if (updatedUser != null) {
+            return ResponseEntity.ok(updatedUser);
+        } else {
+            return ResponseEntity.status(404).body(null);
+        }
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<Users> createUser(@RequestBody Users user) {
+        Users savedUsers = userService.saveUser(user);
+        return ResponseEntity.ok(savedUsers);
+    }
+
     @PostMapping("/login")
-    public ResponseEntity<String> login(@RequestBody Users user) {
-        Users foundUser = userService.findByUsername(user.getNomeUsuario());
-        if (foundUser != null && foundUser.getSenha().equals(user.getSenha())) {
+    public ResponseEntity<?> login(@RequestBody @Valid UserRecordDTO userRecordDTO) {
+        Users foundUser = userService.findByUsername(userRecordDTO.nomeUsuario());
+        String foundPw = userRecordDTO.senha();
+        String foundUserName = userRecordDTO.nomeUsuario();
+
+        if (foundUser != null && foundUser.getSenha().equals(userRecordDTO.senha())) {
             return ResponseEntity.ok("Login successful");
+        } else if (foundPw == null) {
+            return ResponseEntity.status(401).body("Senha é obrigatória");
+        } else if (foundUserName == null) {
+            return ResponseEntity.status(401).body("Nome de usuário é obrigatório");
         } else {
             return ResponseEntity.status(401).body("Invalid credentials");
         }
@@ -43,17 +76,6 @@ public class UserController {
         if (usuario != null) {
             userService.removeById(id);
             return ResponseEntity.ok("Usuário removido com sucesso.");
-        } else {
-            return ResponseEntity.status(401).body("Não foram encotrados dados vinculados a este ID.");
-        }
-    }
-
-    @GetMapping("rescue/{id}")
-    public ResponseEntity<?> getById(@PathVariable(value = "id") UUID id) {
-        Users usuario = userService.findById(id);
-
-        if (usuario != null) {
-            return ResponseEntity.ok(usuario);
         } else {
             return ResponseEntity.status(401).body("Não foram encotrados dados vinculados a este ID.");
         }
