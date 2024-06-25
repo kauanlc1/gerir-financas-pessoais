cecd6eddad8f4a889f6fad23df551ac919bc93fe
Kauan Laureano Cândido <116395890+kauanlc1@users.noreply.github.com>
2024-06-25 11:22:02 -0300

    ♻️ refactor: Atualiza UserService para suportar atualização parcial de usuários
    - Adicionada funcionalidade de atualização parcial de usuários no UserService.
    - Incluído método updateUser para permitir atualização de campos específicos de um usuário.
    - Utilização de BeanWrapper para aplicar atualizações dinamicamente.

--- src/main/java/org/example/service/UserService.java
+++ src/main/java/org/example/service/UserService.java
@@ -2,10 +2,13 @@ package org.example.service;
 
 import org.example.model.Users;
 import org.example.repository.UserRepository;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -33,5 +36,19 @@ public class UserService {
 
     public void removeById(UUID id) {
         userRepository.deleteById(id);
+    }
+
+    public Users updateUser(UUID id, Map<String, Object> updates) {
+        Users existingUser = userRepository.findById(id).orElse(null);
+        if (existingUser != null) {
+            BeanWrapper beanWrapper = new BeanWrapperImpl(existingUser);
+            updates.forEach((key, value) -> {
+                if (beanWrapper.isWritableProperty(key)) {
+                    beanWrapper.setPropertyValue(key, value);
+                }
+            });
+            return userRepository.save(existingUser);
+        }
+        return null;
     }
 }
